164af90906edfe7fb824211bdf7f15678216d5c7
Aleksandra0602 <aleksandraniewiadoma0602@gmail.com>
2021-12-10 12:57:54 +0100

    First view

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -14,47 +15,61 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.pink,
-            title: Text('AppFF'),
-            centerTitle: true,
-          ),
-          body: Column(
-
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.white,
-                    onPrimary: Colors.pink,
-                    minimumSize: Size.fromHeight(50),
-                    maximumSize: Size.fromHeight(75),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    'Log in!',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
+        appBar: AppBar(
+          backgroundColor: Colors.pink,
+          title: Text('AppFF'),
+          centerTitle: true,
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back_rounded, color: Colors.white),
+              iconSize: 24,
+              onPressed: null,
+            ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              height: 250,
+              child: Image.asset('assets/images/AppFF.png', fit: BoxFit.cover),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.pink,
+                  onPrimary: Colors.white,
+                  minimumSize: Size.fromHeight(50),
+                  maximumSize: Size.fromHeight(75),
+                  textStyle: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                onPressed: () {},
+                child: Text('Log in!'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: OutlinedButton(
+                onPressed: () {},
+                child: Text(
+                  'Sign up!',
+                ),
+                style: OutlinedButton.styleFrom(
+                  primary: Colors.pink,
+                  minimumSize: Size.fromHeight(50),
+                  maximumSize: Size.fromHeight(75),
+                  side: BorderSide(
+                      width: 3, color: Colors.pink, style: BorderStyle.solid),
+                  textStyle:
+                      TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.pink,
-                      minimumSize: Size.fromHeight(50),
-                      maximumSize: Size.fromHeight(75),
-                    ),
-                    onPressed: () {},
-                    child: Text('Sign up!',
-                        style:
-                            TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
-              )
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
